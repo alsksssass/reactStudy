437489559ac7fd5ccd1846088b17dda6862c89af
siychoi <152257011+siychoi@users.noreply.github.com>
2024-12-09 13:03:38 +0900

    [#6.1] useEffect

--- MoviePage/siychoi/react-study-siychoi/src/App.js
+++ MoviePage/siychoi/react-study-siychoi/src/App.js
@@ -1,9 +1,22 @@
 import Button from "./Button";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only once");
+  // }
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
   return (
     <div>
       <h1>Welcome back!</h1>
+      <h3>{counter}</h3>
+      <button onClick={onClick}>button</button>
       <Button text={"continue"}/>
     </div>
   );
