c0d629c5e8398c78340efa4b2a3bc673bf3dfdf4
lamodadite <nmb11921@naver.com>
2024-12-09 13:05:29 +0900

    my app

--- MoviePage/jongmlee/src/Button.js
+++ MoviePage/jongmlee/src/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import styles from "./App.module.css";
 
-function Button({ text }) {
-	return <button className={styles.title}>{text}</button>;
+function Button({ onClick, text }) {
+	return <button onClick={onClick} className={styles.title}>{text}</button>;
 }
  export default Button;
